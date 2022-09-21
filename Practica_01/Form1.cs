bde3f0c1dd14a23ad3efe474d40085305843c53e
israel643 <utp0140477@alumno.utpuebla.edu.mx>
2022-09-21 07:39:13 -0500

    Primer comit

---
+++ Practica_01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Arreglo de datos
+        int[] datos = new int[] { 5, 10, 3 };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for(int i = 0; i< datos.Length; i++)
+            {
+                listBox1.Items.Add(datos[i]);
+            }
+        }
+    }
+}

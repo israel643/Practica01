457b6cbb90695760aeba953d644fea43b46d60d3
Israel <utp0140477@alumno.utpuebla.edu.mx>
2022-09-21 20:25:36 -0500

    ALV

--- Practica_01/Form1.Designer.cs
+++ Practica_01/Form1.Designer.cs
@@ -31,36 +31,83 @@ namespace Practica_01
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(86, 82);
+            this.button1.Location = new System.Drawing.Point(32, 37);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 34);
+            this.button1.Size = new System.Drawing.Size(99, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Burbuja";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(316, 82);
+            this.listBox1.Location = new System.Drawing.Point(175, 37);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(153, 199);
+            this.listBox1.Size = new System.Drawing.Size(339, 173);
             this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Quicksort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Carga XML";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(175, 226);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(339, 192);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(546, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +116,10 @@ namespace Practica_01
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Practica_01/Form1.cs
+++ Practica_01/Form1.cs
@@ -22,16 +22,80 @@ namespace Practica_01
 
         }
 
-        //Arreglo de datos
-        int[] datos = new int[] { 5, 10, 3 };
+        //DataSet
+        DataSet ds = new DataSet();
+        public void load()
+        {
+            ds.ReadXml(@"D:\ASP\01\Practica_01\XML\datos.xml");
+            MessageBox.Show("El XML se ha optenido con exito");
+            dataGridView1.DataSource = ds.Tables[0];
+            button3.Enabled = false;
+        }
 
-        private void button1_Click(object sender, EventArgs e)
+        int[] datos = new int[] {10,8,2,7,4,1,9,5,3,6 }; 
+        public void numbers()
         {
             listBox1.Items.Clear();
-            for(int i = 0; i< datos.Length; i++)
+
+            for (int i = 0; i < datos.Length; i++)
+            {
+                for (int j = 0; j < datos.Length - 1; j++)
+                {
+                    if (datos[j] > datos[j + 1])
+                    {
+                        int aux = 0;
+                        aux = datos[j];
+                        datos[j] = datos[j + 1];
+                        datos[j + 1] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i < datos.Length; i++)
             {
                 listBox1.Items.Add(datos[i]);
             }
+        }
+
+        //QuickSort
+        
+        public void Quick(int[] array, int inicio, int final)
+        {
+            /*
+            int x, y, centro;
+            int pivote;
+            centro = (inicio + final)/2;
+            x = inicio;
+            y = final;
+
+            do
+            {
+                while()
+            }
+            */
+        }
+
+                    /*                       Buttons                 */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numbers();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            load();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
